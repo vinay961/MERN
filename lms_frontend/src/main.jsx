5f06ab5ddefe12e257ca5c3af7a14f9a54a8fb43
vinay961 <vinay100rai@gmail.com>
2025-04-24 08:41:16 +0530

    adding toaster

--- lms_frontend/src/main.jsx
+++ lms_frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import './index.css'
 
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
@@ -8,6 +9,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>,
 )
 
